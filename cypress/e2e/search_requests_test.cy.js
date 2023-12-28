68d7e18c22b7dc8ff892532df611779d10fe0b8a
Pavel-VV <vahonin7@mail.ru>
2023-12-28 22:59:44 +0300

    add cypress commands

--- cypress/e2e/search_requests_test.cy.js
+++ cypress/e2e/search_requests_test.cy.js
@@ -1,9 +1,13 @@
 context('Test search requests', () => {
-    it('Visit home page', () => {
-        cy.intercept('GET', 'https://aviasales-api.herokuapp.com/countries', { fixture: 'countries.json' }).as('getCountries')
-        cy.intercept('GET', 'https://aviasales-api.herokuapp.com/cities', { fixture: 'cities.json' }).as('getCities')
-        cy.intercept('GET', 'https://aviasales-api.herokuapp.com/airlines', { fixture: 'airlines.json' }).as('getAirlines')
-        cy.intercept('GET', 'https://aviasales-api.herokuapp.com/prices/cheap*', { fixture: 'tickets.json' })
+    beforeEach(() => {
+        cy.reachThePage()
+        
+        cy.initElements()
+
+        cy.filleTheForm()
+    })
+
+    it('Form submit with correct request params', () => {
         cy.intercept('GET', 'https://aviasales-api.herokuapp.com/prices/cheap*', (req) => {
             console.log(req)
             expect(req.query.currency).to.equal('USD')
@@ -12,56 +16,11 @@ context('Test search requests', () => {
             expect(req.query.destination).to.equal('PAR')
             expect(req.query.origin).to.equal('MOW')
         })
+    })
 
-        cy.visit('http://localhost:9000')
-        cy.get('[data-hook=mainForm]').should('be.visible')
-
-        cy.wait('@getCountries')
-        cy.wait('@getCities')
-        cy.wait('@getAirlines')
-
-        //Form submit with correct request params
-        cy.get('[data-hook=autocompleteOrigin]').as('autocompleteOrigin')
-        cy.get('[data-hook=autocompleteDestination]').as('autocompleteDestination')
-        cy.get('[data-hook=datePickerDepartInput]').as('departInput')
-        cy.get('[data-hook=datePickerDepartWrap] .datepicker-container').as('modalWindow')
-        cy.get('[data-hook=submitButton]').as('submitButton')
-        // cy.get('[data-hook=inputFieldOrigin]').as('inputFieldOrigin')
-        // cy.get('[data-hook=inputFieldDestination]').as('inputFieldDestination')
-
-        // cy.get('@autocompleteOrigin').should('be.visible')
-        // cy.get('@autocompleteOrigin').type('Харьков')
-        // cy.get('.input-origin .autocomplete-content li:first').contains('Харьков,Украина').click()
-        // cy.get('@autocompleteOrigin').should('have.value', 'Харьков,Украина')
-
-        cy.get('@autocompleteOrigin').should('be.visible')
-        cy.get('@autocompleteOrigin').type('Москва')
-        cy.get('.input-origin .autocomplete-content li:first').contains('Москва,Россия').click()
-        cy.get('@autocompleteOrigin').should('have.value', 'Москва,Россия')
-
-        cy.get('@autocompleteDestination').should('be.visible')
-        cy.get('@autocompleteDestination').type('Париж')
-        cy.get('.input-destination .autocomplete-content li:first').contains('Париж,Франция').click()
-        cy.get('@autocompleteDestination').should('have.value', 'Париж,Франция')
-
-        cy.get('@departInput').should('be.visible')
-        cy.get('@departInput').click()
-        cy.get('@modalWindow').should('be.visible')
-
-        cy.get('[data-hook=datePickerDepartWrap] .datepicker-container .is-today').as('isToday')
-        cy.get('[data-hook=datePickerDepartWrap] .datepicker-container .btn-flat').as('modalButtons')
-
-        cy.get('@isToday').click()
-        cy.get('@isToday').should('have.class', 'is-selected')
-        cy.get('@modalButtons').contains('Ok').click()
-
-        cy.get('@submitButton').click()
-
+    it('Tickets display correctly', () => {
         cy.get('[data-hook=ticketsContainer]').as('ticketsContainer')
         cy.get('@ticketsContainer').find('.ticket-card').should('have.length', 2)
-
-        //Tickets display correctly
-
     })
 
     // it('Form submit with correct request params', () => {
@@ -73,4 +32,3 @@ context('Test search requests', () => {
     // })
 })
 
-//27.57
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,52 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('reachThePage', () => {
+    cy.intercept('GET', 'https://aviasales-api.herokuapp.com/countries', { fixture: 'countries.json' }).as('getCountries')
+    cy.intercept('GET', 'https://aviasales-api.herokuapp.com/cities', { fixture: 'cities.json' }).as('getCities')
+    cy.intercept('GET', 'https://aviasales-api.herokuapp.com/airlines', { fixture: 'airlines.json' }).as('getAirlines')
+    cy.intercept('GET', 'https://aviasales-api.herokuapp.com/prices/cheap*', { fixture: 'tickets.json' })
+    
+
+    cy.visit('http://localhost:9000')
+    cy.get('[data-hook=mainForm]').should('be.visible')
+
+    cy.wait('@getCountries')
+    cy.wait('@getCities')
+    cy.wait('@getAirlines')
+})
+
+Cypress.Commands.add('initElements', () => {
+    cy.get('[data-hook=autocompleteOrigin]').as('autocompleteOrigin')
+    cy.get('[data-hook=autocompleteDestination]').as('autocompleteDestination')
+    cy.get('[data-hook=datePickerDepartInput]').as('departInput')
+    cy.get('[data-hook=datePickerDepartWrap] .datepicker-container').as('modalWindow')
+    cy.get('[data-hook=submitButton]').as('submitButton')
+    cy.get('[data-hook=resetButton]').as('resetButton')
+})
+
+Cypress.Commands.add('filleTheForm', () => {
+    cy.get('@resetButton').click()
+    cy.get('@autocompleteOrigin').should('be.visible')
+    cy.get('@autocompleteOrigin').type('Москва')
+    cy.get('.input-origin .autocomplete-content li:first').contains('Москва,Россия').click()
+    cy.get('@autocompleteOrigin').should('have.value', 'Москва,Россия')
+
+    cy.get('@autocompleteDestination').should('be.visible')
+    cy.get('@autocompleteDestination').type('Париж')
+    cy.get('.input-destination .autocomplete-content li:first').contains('Париж,Франция').click()
+    cy.get('@autocompleteDestination').should('have.value', 'Париж,Франция')
+
+    cy.get('@departInput').should('be.visible')
+    cy.get('@departInput').click()
+    cy.get('@modalWindow').should('be.visible')
+
+    cy.get('[data-hook=datePickerDepartWrap] .datepicker-container .is-today').as('isToday')
+    cy.get('[data-hook=datePickerDepartWrap] .datepicker-container .btn-flat').as('modalButtons')
+
+    cy.get('@isToday').click()
+    cy.get('@isToday').should('have.class', 'is-selected')
+    cy.get('@modalButtons').contains('Ok').click()
+
+    cy.get('@submitButton').click()
+})
